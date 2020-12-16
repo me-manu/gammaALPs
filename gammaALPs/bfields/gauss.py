fixed normalization in RM, improving API

gammaALPs/bfields/gauss.py
@@ -389,7 +389,7 @@ class Bgaussian(object):
         # the self._kH factor comes from the substitution t = k / _kH
         return self._B * self._B / 4. * simps(kernel * tt, log(tt), axis=1) * self._kH
 
-    def rotation_measure(self, z, n_el, eta=0.5, nsim=1, n_el0=None):
+    def rotation_measure(self, z, n_el, Bscale=None, nsim=1):
         """
         Calculate the rotation measure of a
         random Gaussian field.
@@ -407,10 +407,9 @@ class Bgaussian(object):
         nsim: int
             number of B-field simulations
 
-        n_el0: None or float
-            normalization for B field scaling with electron density,
-            B = B0 * (n_el(r) / n_el0)^eta.
-            If none, n_el[0] will be used.
+        Bscale: array-like or None
+            if given, this array is multiplied with the B field for an additional scaling depending on r,
+            e.g., (n_el(r) / n_el(0))^eta
 
 
 
@@ -419,9 +418,8 @@ class Bgaussian(object):
         Rotation measure for each simulated B field as an `~numpy.ndarray` if nsim > 1
         or as a scalar if nsim=1
         """
-
-        if n_el0 is None:
-            n_el0 = n_el[0]
+        if Bscale is None:
+            Bscale = np.ones_like(z)
 
         seed(self.seed)
         # if seed is integer
@@ -445,7 +443,7 @@ class Bgaussian(object):
             # the B field in sqrt
             self.new_random_numbers()
             B = np.sqrt(2.) * self.Bgaus(z)
-            kernel.append(B * (n_el / n_el0)**eta * n_el)
+            kernel.append(B * Bscale * n_el)
 
         # restore old seed
         if seeds is not None: