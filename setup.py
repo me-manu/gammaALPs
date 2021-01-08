215f575cfda2174659acf934c561af84aaa67676
me-manu <me.manu@gmx.net>
2021-01-08 11:36:29 +0100

    corrected license

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     author_email='me.manu@gmx.net',
 
     # Choose your license
-    license='BSD-3',
+    license='BSD',
 
     # See https://PyPI.python.org/PyPI?%3Aaction=list_classifiers
     classifiers=[
