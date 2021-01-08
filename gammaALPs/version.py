3d4402a42ade850473403a31b5ce3b96a8c56d0a
me-manu <me.manu@gmx.net>
2021-01-08 12:09:22 +0100

    tweaking versioning

--- gammaALPs/version.py
+++ gammaALPs/version.py
@@ -31,76 +31,159 @@
 #
 #   include RELEASE-VERSION
 
+import os
+import subprocess
+from subprocess import check_output
+
 __all__ = ("get_git_version")
 
-from subprocess import Popen, PIPE
+_refname = '$Format: %D$'
+_tree_hash = '$Format: %t$'
+_commit_info = '$Format:%cd by %aN$'
+_commit_hash = '$Format: %h$'
+
+
+def capture_output(cmd, dirname):
+
+    p = subprocess.Popen(cmd,
+                         stdout=subprocess.PIPE,
+                         stderr=subprocess.PIPE,
+                         cwd=dirname)
+    p.stderr.close()
+
+    output = p.stdout.readlines()
+
+    if not output:
+        return None
+    else:
+        return output[0].strip()
+
+
+def render_pep440(vcs):
+    """
+    Convert git release tag into a form that is PEP440 compliant.
+    Remove commit hashes
+    """
+
+    if vcs is None:
+        return None
+
+    tags = vcs.split('-')
+
+    # Bare version number
+    if len(tags) == 1:
+        return tags[0]
+    else:
+        return tags[0] + '+' + '.'.join([t for t in tags[1:] if not 'g' in t])
 
 
 def call_git_describe(abbrev=4):
+
+    dirname = os.path.abspath(os.path.dirname(__file__))
+
     try:
-        p = Popen(['git', 'describe', '--abbrev=%d' % abbrev],
-                  stdout=PIPE, stderr=PIPE)
-        p.stderr.close()
-        line = p.stdout.readlines()[0]
-        return line.strip()
+        has_git_tree = capture_output(['git', 'rev-parse',
+                                       '--is-inside-work-tree'], dirname)
+    except:
+        return None
+
+    if not has_git_tree:
+        return None
+
+    try:
+        line = check_output(['git', 'describe', '--abbrev=%d' % abbrev,
+                             '--dirty', '--tags'], cwd=dirname)
+
+        return line.strip().decode('utf-8')
 
     except:
         return None
+
+
+def read_release_keywords(keyword):
+
+    refnames = keyword.strip()
+    if refnames.startswith("$Format"):
+        return None
+
+    refs = set([r.strip() for r in refnames.strip("()").split(",")])
+    TAG = "tag: "
+    tags = set([r[len(TAG):] for r in refs if r.startswith(TAG)])
+    if not tags:
+        return None
+    return sorted(tags)[-1]
 
 
 def read_release_version():
+    """Read the release version from ``_version.py``."""
+    import re
+    dirname = os.path.abspath(os.path.dirname(__file__))
+
     try:
-        f = open("RELEASE-VERSION", "r")
+        f = open(os.path.join(dirname, "_version.py"), "rt")
+        for line in f.readlines():
 
-        try:
-            version = f.readlines()[0]
-            return version.strip()
-
-        finally:
-            f.close()
+            m = re.match("__version__ = '([^']+)'", line)
+            if m:
+                ver = m.group(1)
+                print ("m", m, m.group(1), ver)
+                return ver
 
     except:
         return None
 
+    return None
+
 
 def write_release_version(version):
-    f = open("RELEASE-VERSION", "w")
-    f.write("%s\n" % version)
+    """Write the release version to ``_version.py``."""
+    dirname = os.path.abspath(os.path.dirname(__file__))
+    f = open(os.path.join(dirname, "_version.py"), "wt")
+    f.write("__version__ = '%s'\n" % version)
     f.close()
 
 
 def get_git_version(abbrev=4):
-    # Read in the version that's currently in RELEASE-VERSION.
 
+    # Read in the version that's currently in _version.py.
     release_version = read_release_version()
+    print("rv",release_version)
 
     # First try to get the current version using “git describe”.
+    git_version = call_git_describe(abbrev)
+    git_version = render_pep440(git_version)
+    print("gv", git_version)
 
-    version = call_git_describe(abbrev)
+    # Try to deduce the version from keyword expansion
+    keyword_version = read_release_keywords(_refname)
+    print("kv1", keyword_version)
+    keyword_version = render_pep440(keyword_version)
+    print("kv2", keyword_version)
 
     # If that doesn't work, fall back on the value that's in
-    # RELEASE-VERSION.
-
-    version = version.decode("utf-8")
-
-    if version is None:
+    # _version.py.
+    if git_version is not None:
+        version = git_version
+    elif release_version is not None:
         version = release_version
+    elif keyword_version is not None:
+        version = keyword_version
+    else:
+        version = 'unknown'
 
     # If we still don't have anything, that's an error.
-
     if version is None:
         raise ValueError("Cannot find the version number!")
 
     # If the current version is different from what's in the
-    # RELEASE-VERSION file, update the file to be current.
-
-    if version != release_version:
+    # _version.py file, update the file to be current.
+    if version != release_version and version != 'unknown':
         write_release_version(version)
 
     # Finally, return the current version.
-
     return version
 
 
 if __name__ == "__main__":
     print(get_git_version())
+
