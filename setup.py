c14ee540c00092091eea6b3dc9e154abee934d61
me-manu <me.manu@gmx.net>
2021-02-08 16:09:50 +0100

    removed python 2.7 from setup.py

--- setup.py
+++ setup.py
@@ -33,9 +33,9 @@ setup(
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering :: Astronomy',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
